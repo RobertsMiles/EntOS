eda900011bbdf10705b2227c7c3156c2a57f911c
Miles Roberts <milesrobertsmain@gmail.com>
2024-10-23 01:42:46 -0500

    added help command

---
+++ help.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "help.h"
+
+void help() {
+    printf("This is the ENT Operating System, a completely impractical suite of software that I am writing for fun. ENT is a recursive acronym that stands for \"ENT's Not TempleOS\".\n");
+}
--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include "help.h"
 
 int main() {
+    char username[9] = "User";
+    char hostname[9] = "ENTOS";
+    char inputBuffer[99];
 
-    return 0;
+    while (1) {
+        printf("%s@%s: ", username, hostname);
+        scanf("%s", inputBuffer);
+
+        if (!strcmp(inputBuffer, "help")) help();
+        else if (!strcmp(inputBuffer, "quit")) return 0;
+        else printf("Invalid argument!\n");
+    }
 }
